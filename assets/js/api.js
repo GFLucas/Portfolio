f076f9a81b060f46c8b9d6554eb9c3fe8475fb07
Lucas Guilherme <lucassgfcosta@gmail.com>
2024-01-05 11:05:49 -0400

    commit

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/GFLucas/Portfolio/master/data/profile.json';
+    const url = '';
     const fetching = await fetch(url);
     return await fetching.json()
 }
